f58e440f21ac3a9eab67c5a03adb7ef1f1fb8d7d
Elizeu Ribeiro Junior <elizeuri@br.ibm.com>
2020-03-11 17:49:17 -0300

    lógica de calculo finalizada

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const nome = "silvana";
-const sexo = "F";
+const sexo = "M";
 const idade = 48;
-const contribuicao = 23;
+const contribuicao = 34;
 
-if ((sexo == "f") && (contribuicao >= 35)){ 
-    if ((contribuicao + idade))
-  }else {
-    console.log("Seu tempo de contribuição ainda não atingiu o minimo necessário")
-  }
-}else{
-  
+if (((sexo == "F") && (contribuicao >= 30)) || ((sexo == "M") && (contribuicao >=35))) { 
+  if (((sexo == "F") && (contribuicao + idade >=85)) || ((sexo == "M") && (contribuicao + idade >= 95))) { 
+    console.log(nome, ", você pode se aposentar!") 
+
+  } else {
+    console.log (nome,", você ainda não possui a soma de periodo necessária para se aposentar")
+  } 
+} else {
+    console.log (nome,", você ainda não possui tempo de contribuição para se aposentar")
 }
+  // condição 1 passou, porem condição 2 não passou
+
+
+
+
+
+
+
+
+
+
+/* if ((sexo == "f") && (contribuicao >= 35)) || ((sexo == "m") && (contribuicao >= 30)){ 
+    if ((contribuicao + idade)){
+      // condição 1 e 2 true
+    console.log(contribuicao + idade)  
+  } else {
+    console.log("Seu tempo de contribuição ainda não atingiu o minimo necessário")
+    // condição 1 passou, porém condição 2 não passou
+  }
+} else {
+  console.log()
+} */
