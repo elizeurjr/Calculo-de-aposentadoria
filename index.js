0d8013dc97c6b0bc4614ba25abe0afdd8024292a
Elizeu Ribeiro Junior <elizeuri@br.ibm.com>
2020-03-11 17:02:02 -0300

    implementação das condições

---
+++ index.js
@@ -0,0 +1,13 @@
+const nome = "silvana";
+const sexo = "F";
+const idade = 48;
+const contribuicao = 23;
+
+if ((sexo == "f") && (contribuicao >= 35)){ 
+    if ((contribuicao + idade))
+  }else {
+    console.log("Seu tempo de contribuição ainda não atingiu o minimo necessário")
+  }
+}else{
+  
+}
